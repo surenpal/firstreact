1c7ce5c1083102ec5c9f8b5f8a5d72e0a19f55f3
surenpal <surenpal666@gmail.com>
2025-12-20 00:24:59 +0900

    navbar is unable to correct

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 
-function Card(props) {
-    return (
-        <h1>Hello World there, {props.name} and age is {props.age}</h1>
-
-    );
+function Card({ name, age }) {
+  return (
+    <div style={{ border: "1px solid #ccc", padding: "10px", margin: "10px" }}>
+      <h2>{name}</h2>
+      <p>Age: {age}</p>
+    </div>
+  );
 }
 
 export default Card;
