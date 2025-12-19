8c8d30e597430d2f3ed211ee5fe62d9e6ccdc910
surenpal <surenpal666@gmail.com>
2025-12-19 21:56:20 +0900

    i have added props funtion to the component and pass it to the parent app.jsx file

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import './App.css';
+import Card from './components/card.jsx'
 
 export const App = () => {
   return (
     <div>
-      <h1>Hello World</h1>
+      <Card name="Surendra" age={25} />
+      <Card name="Dhiren" age={30} />
+      <Card name="Bhiren" age={35} />
     </div>
   )
 }
---
+++ src/components/card.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+function Card(props) {
+    return (
+        <h1>Hello World there, {props.name} and age is {props.age}</h1>
+
+    );
+}
+
+export default Card;
